063a46ed1eba076d4116804f46358f9221718813
Callum Flack <callumflack@gmail.com>
2018-08-20 18:48:42 +1000

    cleanup

--- apollo/client-configs/default.js
+++ apollo/client-configs/default.js
@@ -1,8 +1,7 @@
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
-// Shopify endpoint
-// const GRAPHCMS_API = "https://api-useast.graphcms.com/v1/cjiacyow100ob01eqwnghonw2/master";
+// Shopify access
 const SHOPIFY_API = "https://cfdd.myshopify.com/api/graphql";
 const SHOPIFY_STOREFRONT_ACCESS_TOKEN = "811b7025f726d038cb26fdf4edb504b9";
 
