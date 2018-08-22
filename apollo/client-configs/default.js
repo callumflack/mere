be04640922d8a41c1fddade357bc1625619e0ed2
Callum Flack <callumflack@gmail.com>
2018-08-22 18:44:25 +1000

    shopify: use new dev partner config

--- apollo/client-configs/default.js
+++ apollo/client-configs/default.js
@@ -2,8 +2,8 @@ import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 // Shopify access
-const SHOPIFY_API = "https://cfdd.myshopify.com/api/graphql";
-const SHOPIFY_STOREFRONT_ACCESS_TOKEN = "811b7025f726d038cb26fdf4edb504b9";
+const SHOPIFY_API = "https://mere2.myshopify.com/api/graphql";
+const SHOPIFY_STOREFRONT_ACCESS_TOKEN = "85518a23492805f72d32ad891e5357fd";
 
 export default () => ({
   link: new HttpLink({
